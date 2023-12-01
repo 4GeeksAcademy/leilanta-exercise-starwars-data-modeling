c379a7b3c16831b5893fe87c3419ed4c9100fb26
Leilani Tanimoto Abad <135250913+Leilanta@users.noreply.github.com>
2023-12-01 19:32:42 +0000

    corrections

--- src/models.py
+++ src/models.py
@@ -1,19 +1,38 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, PrimaryKey
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-
 Base = declarative_base()
-
-#  class Person(Base):
-# #     __tablename__ = 'person'
-# #     # Here we define columns for the table person
-# #     # Notice that each column is also a normal Python instance attribute.
-# #     id = Column(Integer, primary_key=True)
-# #     name = Column(String(250), nullable=False)
-
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+class Person (Base):
+    __tablename__ = 'Person'
+    id = Column (Integer, primary_key=True)
+    name =  Column (String(250), nullable=False)
+    height = Column(Float , nullable=False)
+    mass = Column (Float , nullable=False)
+    hair_color = Column (String(250) , nullable=False)
+    skin_color = Column (String(250) , nullable=False)
+class Planet(Base):
+    __tablename__ = 'Planet'
+    id = Column (Integer, primary_key=True)
+    name =  Column (String(250), nullable=False)
+    population = Column(Integer , nullable=False)
+    terrain = Column (String(250), nullable=False)
+    climate = Column (String(250), nullable=False)
+class Starship(Base):
+    __tablename__ = 'Starship'
+    id = Column (Integer, primary_key=True)
+    name =  Column (String(250), nullable=False)
+    model = Column (String(250), nullable=False)
+    manufacturer = Column (String(250), nullable=False)
+    cargo_capacity = Column (Integer, nullable=False)
 # class Address(Base):
 #     __tablename__ = 'address'
 #     # Here we define columns for the table address.
@@ -24,40 +43,8 @@ Base = declarative_base()
 #     post_code = Column(String(250), nullable=False)
 #     person_id = Column(Integer, ForeignKey('person.id'))
 #     person = relationship(Person)
-
-class Person (Base):
-    __tablename__ = 'Person'
-    id = Column (Integer, primary_key=True)
-    name =  Column (String(250), nullable=False)
-    height = Column(Float , nullable=False)
-    mass = Column (Float , nullable=False)
-    hair_color = Column (String(250) , nullable=False)
-    skin_color = Column (String(250) , nullable=False)
-
-class Planet(Base):
-    __tablename__ = 'Planet'
-    id = Column (Integer, primary_key=True)
-    name =  Column (String(250), nullable=False)
-    population = Column(Integer , nullable=False)
-    terrain = Column (String(250), nullable=False)
-    climate = Column (String(250), nullable=False)
-
-class Starship(Base):
-    __tablename__ = 'Starship'
-    id = Column (Integer, primary_key=True)
-    name =  Column (String(250), nullable=False)
-    model = Column (String(250), nullable=False)
-    manufacturer = Column (String(250), nullable=False)
-    cargo_capacity = Column (Integer, nullable=False)
-
-
-
-
-
-
     def to_dict(self):
         return {}
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
 
